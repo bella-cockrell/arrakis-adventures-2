0eeb0fcc969831e37610a5643aa8a84519e7f8e9
bella-cockrell <heybellac@gmail.com>
2020-08-02 21:22:00 +0100

    Adding header animation

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ function App() {
   return (
     <div>
       <header className="header">
-        <div className="logo-box">
+        <div className="logo_box">
           <img src={logo} alt="logo" className="logo" />
+        </div>
+        <div className="text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary_main">Outdoors</span>
+            <span className="heading-primary_sub">is where life happens</span>
+          </h1>
         </div>
       </header>
     </div>
