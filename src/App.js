c037bdc92d3c7668edff0603143b14cf60e82ca7
bella-cockrell <heybellac@gmail.com>
2020-08-02 21:45:20 +0100

    Adding button to header

--- src/App.js
+++ src/App.js
@@ -15,8 +15,12 @@ function App() {
             <span className="heading-primary_main">Outdoors</span>
             <span className="heading-primary_sub">is where life happens</span>
           </h1>
+          <a href="#tours" className="btn btn-white">
+            Discover our tours
+          </a>
         </div>
       </header>
+      <section className="tours" id="tours"></section>
     </div>
   )
 }
